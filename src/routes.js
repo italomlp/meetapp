fd8724d2e0511f0026fedc0ec0d9a5f9523d8038
Italo Menezes <italomlporoca@hotmail.com>
2019-08-15 13:11:43 -0300

    Add model loader

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'hello world' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Italo Menezes',
+    email: 'italomlporoca@hotmail.com',
+    password_hash: '123123'
+  })
+
+  return res.json(user);
 });
 
 export default routes;
